22bdbc503d55529f93bbffb4beb2ab27a803c749
alexis0592 <yefry.calderon@udea.edu.co>
2014-09-19 18:48:59 -0500

    Creating the project

---
+++ Redeactiva/gen/co/gov/coldeportes/redeactiva/redapptiva/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package co.gov.coldeportes.redeactiva.redapptiva;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
