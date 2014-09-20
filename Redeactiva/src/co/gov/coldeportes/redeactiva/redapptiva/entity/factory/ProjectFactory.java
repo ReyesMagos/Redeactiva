8ad8f4e3bd779d586856608d269a90a8576d00f1
alexis0592 <yefry.calderon@udea.edu.co>
2014-09-20 07:19:55 -0500

    adding new things

---
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/entity/factory/ProjectFactory.java
@@ -0,0 +1,21 @@
+package co.gov.coldeportes.redeactiva.redapptiva.entity.factory;
+
+public class ProjectFactory {
+	
+	public static ProjectFactory projectInstance;
+	
+	private ProjectFactory(){
+		
+	}
+	
+	public static ProjectFactory getInstance(){
+		if(projectInstance == null){
+			projectInstance = new ProjectFactory();
+		}
+		
+		return projectInstance;
+	}
+	
+	
+
+}
