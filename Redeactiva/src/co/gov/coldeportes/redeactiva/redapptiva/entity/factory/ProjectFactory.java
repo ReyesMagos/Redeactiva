c93e96235d88da532590d9787cc0a65d6d714832
alexis0592 <yefry.calderon@udea.edu.co>
2014-09-20 12:41:38 -0500

    web consume of projects

--- Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/entity/factory/ProjectFactory.java
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/entity/factory/ProjectFactory.java
@@ -10,56 +10,60 @@ import org.json.JSONObject;
 import co.gov.coldeportes.redeactiva.redapptiva.entity.model.Projects;
 
 public class ProjectFactory {
-	
+
 	public static ProjectFactory projectInstance;
-	
-	private List<Projects>projectsList;
-	
-	private ProjectFactory(){
-		
+
+	private List<Projects> projectsList;
+
+	private ProjectFactory() {
+
 	}
-	
-	public static ProjectFactory getInstance(){
-		if(projectInstance == null){
+
+	public static ProjectFactory getInstance() {
+		if (projectInstance == null) {
 			projectInstance = new ProjectFactory();
 		}
-		
+
 		return projectInstance;
 	}
-	
-	public void fillProjects(JSONArray jsonArray){
+
+	public void fillProjects(JSONArray jsonArray) {
 		JSONObject jsonObject;
 		this.projectsList = new ArrayList<Projects>();
-		
-		if(jsonArray == null){
+
+		if (jsonArray == null || jsonArray.length() == 0) {
 			return;
 		}
-		
+
 		try {
-			for(int i = 0; i < jsonArray.length(); i++){
+			for (int i = 0; i < jsonArray.length(); i++) {
 				jsonObject = jsonArray.getJSONObject(i);
 				Projects projects = new Projects();
 				projects.setRowKey(jsonObject.getString("RowKey"));
 				projects.setPartitionKey((jsonObject.getString("PartitionKey")));
 				projects.setTipo((jsonObject.getString("tipo")));
-				projects.setNombrePrograma((jsonObject.getString("nombreprograma")));
-				projects.setDescripcionPrograma((jsonObject.getString("descripcionprograma")));
+				projects.setNombrePrograma((jsonObject
+						.getString("nombreprograma")));
+				projects.setDescripcionPrograma((jsonObject
+						.getString("descripcionprograma")));
 				projects.setTipoEntidad((jsonObject.getString("tipodeentidad")));
-				projects.setCodigoDepartamento((jsonObject.getString("codigodepartamento")));
-				projects.setCodigoMunicipio((jsonObject.getString("codigomunicipio")));
-				projects.setPersonaContacto(jsonObject.getString("personacontacto"));
-				projects.setDireccionMunicipio(jsonObject.getString("direccionmunicipio"));
+				projects.setCodigoDepartamento((jsonObject
+						.getString("codigodepartamento")));
+				projects.setCodigoMunicipio((jsonObject
+						.getString("codigomunicipio")));
+				projects.setPersonaContacto(jsonObject
+						.getString("personacontacto"));
+				projects.setDireccionMunicipio(jsonObject
+						.getString("direccionmunicipio"));
 				projects.setTelefono(jsonObject.getString("telefono"));
 				projects.setEmail(jsonObject.getString("email"));
 				projects.setPaginaWeb(jsonObject.getString("paginaweb"));
-				
-				
+				this.projectsList.add(projects);
+
 			}
 		} catch (JSONException e) {
 			// TODO: handle exception
 		}
 	}
-	
-	
 
 }
--- Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/service/dao/ProjectDAO.java
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/service/dao/ProjectDAO.java
@@ -1,6 +1,19 @@
 package co.gov.coldeportes.redeactiva.redapptiva.service.dao;
 
-public class ProjectDAO {
+import org.json.JSONArray;
 
-	
+import co.gov.coldeportes.redeactiva.redapptiva.entity.factory.ProjectFactory;
+
+public class ProjectDAO extends AbstractDAO {
+
+	public void executeAsyncTaskDAO() {
+		// TODO Auto-generated method stub
+		super.executeAsyncTaskDAO("http://servicedatosabiertoscolombia."
+				+ "cloudapp.net/v1/Coldeportes/portafolioproyectos?$format=json");
+	}
+
+	public void fillProjects(JSONArray array) {
+		ProjectFactory.getInstance().fillProjects(array);
+	}
+
 }
