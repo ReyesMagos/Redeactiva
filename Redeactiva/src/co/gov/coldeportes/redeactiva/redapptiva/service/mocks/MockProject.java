fa01ff86e41842e27476bdd0015cf6901398e537
alexis0592 <yefry.calderon@udea.edu.co>
2014-09-20 09:21:55 -0500

    adding methods to asynctask

--- Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/service/mocks/MockProject.java
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/service/mocks/MockProject.java
@@ -1,8 +1,14 @@
 package co.gov.coldeportes.redeactiva.redapptiva.service.mocks;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MockProject {
 
-	
+	private String portafolioProyectos;
+
+	public void setPortafolioProyectos() {
+		portafolioProyectos = "{d: [116] " +
+				"0:  {PartitionKey: \"830e8583-31d3-466a-a2ad-f5197a538507\"";
+	}
 }
