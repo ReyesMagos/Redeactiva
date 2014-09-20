4558fb7829e8ddd0045d2da9d230cd1258043cfa
alexis0592 <yefry.calderon@udea.edu.co>
2014-09-20 08:59:59 -0500

    Adding asynctask and abstractDAO

---
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/service/dao/AbstractDAO.java
@@ -0,0 +1,17 @@
+package co.gov.coldeportes.redeactiva.redapptiva.service.dao;
+
+import co.gov.coldeportes.redeactiva.redapptiva.service.asynctask.AsyncTaskDAO;
+
+import com.parse.ParseQuery;
+
+public abstract class AbstractDAO {
+
+	public void executeAsyncTaskDAO(String url){
+		AsyncTaskDAO asyncTaskDAO = new AsyncTaskDAO(this);
+		asyncTaskDAO.execute(url);
+	}
+	
+	public void executeAsyncTaskParse(ParseQuery parsequery){
+		
+	}
+}
