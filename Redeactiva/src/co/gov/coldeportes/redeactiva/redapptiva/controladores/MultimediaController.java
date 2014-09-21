a6640a4684eca876649c4f737eacddf987ad13f4
reyesmagos <reyesmagossoft.co@gmail.com>
2014-09-21 00:32:10 -0500

    youtube view

--- Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/controladores/MultimediaController.java
+++ Redeactiva/src/co/gov/coldeportes/redeactiva/redapptiva/controladores/MultimediaController.java
@@ -4,6 +4,11 @@ import co.gov.coldeportes.redeactiva.redapptiva.MultimediaActivity;
 import co.gov.coldeportes.redeactiva.redapptiva.entity.factory.MultimediaFactory;
 import co.gov.coldeportes.redeactiva.redapptiva.entity.model.adaptadores.CustomAdapterMultimedia;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MultimediaController extends AbstractController {
 
@@ -20,6 +25,20 @@ public class MultimediaController extends AbstractController {
 
 		multimediaActivity.getListaMultimedia().setAdapter(
 				customAdapterMultimedia);
+		multimediaActivity.getListaMultimedia().setOnItemClickListener(
+				new OnItemClickListener() {
+
+					@Override
+					public void onItemClick(AdapterView<?> arg0, View arg1,
+							int arg2, long arg3) {
+						// TODO Auto-generated method stub
+						getActivity().startActivity(
+								new Intent(Intent.ACTION_VIEW, Uri
+										.parse(MultimediaFactory.getInstance()
+												.getListaMultimedia().get(arg2)
+												.getLink())));
+					}
+				});
 	}
 
 }
